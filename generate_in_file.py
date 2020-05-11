e9c2a7a4aa8ab6dd9823a0bba171e78d72a3254e
Ollie Chick <och26@uclive.ac.nz>
2020-05-11 23:25:27 +1200

    Remove print statement

--- generate_in_file.py
+++ generate_in_file.py
@@ -3,7 +3,6 @@ from file_io import write_to_file
 
 
 def main():
-    print(os.environ)
     username = os.environ['ig_username']
     password = os.environ['ig_password']
     output = '\n'.join([username, password, 'n', '1'])
