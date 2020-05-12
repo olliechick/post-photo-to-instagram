a54f7012296a81a4e74593e976d8dff85ed7d6f9
Ollie Chick <och26@uclive.ac.nz>
2020-05-12 18:13:14 +1200

    Fix getting post list from CSV, and add "support" for any extension
    Previously it assumed posts were only `jpg`s

--- post-photo-to-instagram.py
+++ post-photo-to-instagram.py
@@ -7,40 +7,45 @@ from datetime import date
 import requests
 from instabot import Bot
 
-IMAGE_FILENAME = "image.jpg"
+PLACEHOLDER = "placeholder"
 
 
 def main():
-
     # Get image URL
-    posts = csv.reader(requests.get(os.environ['input_url']).text.splitlines())
+    reader = csv.reader(requests.get(os.environ['input_url']).text.splitlines())
+    posts = []
+    for post in reader:
+        posts.append(post)
+
     random.seed(date.today())
     i = random.randrange(len(posts))
+    print(posts[i])
     filename, caption = posts[i]
     image_url = os.environ['image_host'] + filename
+    extension = filename.split('.')[-1]
+
+    placeholder_filename = f"{PLACEHOLDER}.{extension}"
 
     # Download image
     try:
-        urllib.request.urlretrieve(image_url, IMAGE_FILENAME)
+        urllib.request.urlretrieve(image_url, placeholder_filename)
     except Exception as e1:
         try:
-            urllib.request.urlretrieve(image_url, IMAGE_FILENAME)
+            urllib.request.urlretrieve(image_url, placeholder_filename)
         except Exception as e2:
             print(f"Errors while downloading image': {e1}, {e2}")
 
     bot = Bot()
     bot.login(username=os.environ['ig_username'], password=os.environ['ig_password'])
 
-    pic = IMAGE_FILENAME
-
     try:
         print("Uploading post with caption: " + caption)
 
-        bot.upload_photo(pic, caption=caption)
+        bot.upload_photo(placeholder_filename, caption=caption)
         if bot.api.last_response.status_code != 200:
             print(bot.api.last_response)
 
-        os.remove(IMAGE_FILENAME + ".REMOVE_ME")
+        os.remove(placeholder_filename + ".REMOVE_ME")
 
     except Exception as e:
         print(str(e))
